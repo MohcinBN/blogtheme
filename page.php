9b6b608f9949cd27ac913fe1633f0656574fd127
MohcinBN <rapmabiz@gmail.com>
2020-11-28 21:00:22 +0100

    Add static page functionality

--- page.php
+++ page.php
@@ -1,1 +1,15 @@
-TEST
+<?php get_header(); ?>
+
+  <!-- Main Content -->
+<div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();       
+        the_content(); 
+        endwhile; endif;
+        ?>
+      </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
